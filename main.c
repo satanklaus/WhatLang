29b65a5a4ecad1cfebd862555c565b88ee7aa85c
satanklaus <empated@gmail.com>
2014-11-17 06:09:33 +0300

    little changes

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <X11/XKBlib.h>
 #include <stdio.h>
 
+#define NO_ERR 0
 #define ERR_OPENDISPLAY 1
 #define ERR_GETSTATE 2
 #define ERR_GETKBD 3
 
+
 int main(int argc, char *argv[])
 {
-int err;
+int err = NO_ERR;
 Display *display;
 XkbDescPtr keyboard;
 XkbStateRec state;
@@ -18,10 +20,11 @@ if (XkbGetState(display, XkbUseCoreKbd, &state)) {err = ERR_GETSTATE; goto err_g
 if (!(keyboard = XkbGetKeyboard(display,XkbAllComponentsMask,XkbUseCoreKbd))) {err = ERR_GETKBD; goto err_get_keyboard;}
 printf("%s\n",XGetAtomName(display,keyboard->names->groups[state.locked_group]));
 XkbFreeKeyboard(keyboard,XkbAllComponentsMask,1);
-return 0;
+goto clean_exit;
 err_get_keyboard:
 err_get_state:
 XCloseDisplay(display);
 err_open: 
+clean_exit:
 return err;
 }
